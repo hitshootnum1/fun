6c0931877a0f6bcc3d14edbf6a0df17922cee03b
hitshootnum1 <hitshootnum1@gmail.com>
2017-02-15 17:02:41 +0700

    correct quicksort

--- src/main/java/algorithm/sorting/QuickSort.java
+++ src/main/java/algorithm/sorting/QuickSort.java
@@ -16,7 +16,7 @@ public class QuickSort extends SortingAlgorithm {
     void quickSort(int start, int end) {
         if (start < end) {
             int splitPoint = partition(start, end);
-            quickSort(start, splitPoint);
+            quickSort(start, splitPoint - 1);
             quickSort(splitPoint + 1, end);
         }
     }
@@ -27,34 +27,26 @@ public class QuickSort extends SortingAlgorithm {
         int leftIndex = start + 1;
         int rightIndex = end;
 
-        while(leftIndex < rightIndex) {
-            // if left side > pivot and right sight > pivot, they should be swapped
-            if (inputArray[leftIndex] > pivot && inputArray[rightIndex] < pivot) {
-                swap(leftIndex, rightIndex);
-            }
-
+        while(true) {
             // if left side < pivot, satisfy the clause, increase leftIndex to move to the next position
-            while (inputArray[leftIndex] < pivot && leftIndex < end) {
+            while (leftIndex <= rightIndex && inputArray[leftIndex] <= pivot) {
                 leftIndex++;
             }
 
             // if right side > pivot, satisfy the clause, increase right Index to move to the prev position
-            while (inputArray[rightIndex] > pivot && rightIndex > start) {
+            while (rightIndex >= leftIndex && inputArray[rightIndex] >= pivot) {
                 rightIndex--;
             }
+
+            // if left side > pivot and right sight > pivot, they should be swapped
+            if (rightIndex < leftIndex) {
+                break;
+            } else {
+                swap(leftIndex, rightIndex);
+            }
         }
-//        if(leftIndex >= rightIndex) leftIndex--;
-        int minIndex = leftIndex < rightIndex ? leftIndex : rightIndex;
-        swap(start, minIndex);
 
-        // swap pivot with the most right index.
-//        if(leftIndex != end && leftIndex != start + 1)
-//            swap(start, --leftIndex);
-//        else if(leftIndex >= rightIndex)
-//            swap(start, --leftIndex);
-//        else
-//            swap(start, leftIndex);
-
-        return minIndex;
+        swap(start, rightIndex);
+        return rightIndex;
     }
 }
--- src/test/java/algorithm/sorting/QuickSortTest.java
+++ src/test/java/algorithm/sorting/QuickSortTest.java
@@ -1,7 +1,10 @@
 package algorithm.sorting;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -42,8 +45,8 @@ public class QuickSortTest {
     public void partition3() throws Exception {
         QuickSort quickSort = new QuickSort();
         quickSort.inputArray = new int [] {1, 3, 2};
-        assertThat(quickSort.partition(0, 2), is(1));
-        assertThat(quickSort.inputArray, is(new int []{3, 1, 2}));
+        assertThat(quickSort.partition(0, 2), is(0));
+        assertThat(quickSort.inputArray, is(new int []{1, 3, 2}));
 
     }
 
@@ -64,11 +67,26 @@ public class QuickSortTest {
     }
 
     @Test
-    @Ignore
-    public void sort() throws Exception {
+    public void quickSortFixed() throws Exception {
         QuickSort quickSort = new QuickSort();
-        int [] input = new int [] {4, 3, 2, 1};
+        int [] input = new int [] {3, 2, 2};
         int [] output = quickSort.sort(input);
-        assertThat(output, is(new int [] {1, 2, 3, 4}));
+        assertThat(output, is(new int [] {2, 2, 3}));
+    }
+
+    @Test
+    public void quickSortRandomized() throws Exception {
+        QuickSort quickSort = new QuickSort();
+        BubbleSort bubbleSort = new BubbleSort();
+
+        int[] randomInput = new int[100];
+        Random ran = new Random();
+        for(int i = 0; i < randomInput.length; i++) {
+            randomInput[i] = ran.nextInt((int)System.currentTimeMillis() % 10);
+        }
+
+        int[] bubbleOutput = bubbleSort.sort(randomInput);
+        int[] actualOutput = quickSort.sort(randomInput);
+        Assert.assertThat(actualOutput, is(bubbleOutput));
     }
 }
